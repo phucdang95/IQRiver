631c0bf1c5f2157497de6610b0bc70864d573829
Phuc Dang <phucdang95@yahoo.com>
2017-07-25 17:22:31 -0400

    Add files via upload

--- car_rental/header.php
+++ car_rental/header.php
@@ -4,29 +4,29 @@
 ?>
 <header>
 			<div class="wrapper">
-				<h1 class="logo">Luxury Rental</h1>
-				
+				<h1 class="logo">Luxury Rental Inc.</h1>
+
 				<nav>
 
 					<?php
 						if(!$_SESSION['email'] && (!$_SESSION['pass'])){
 					?>
-                    
+
 					<ul>
 						<li><a href="index.php">Home</a></li>
-						<li><a href="index.php">Rent Cars</a></li>
+						<li><a href="">About Us</a></li>
 						<li><a href="#">Contact Us</a></li>
 					</ul>
-                   
-                   
+
+
 					<a href="account.php">Customer Login</a> /
 					<a href="login.php">Admin Login</a>
-                   
+
 
 					<?php
 						} else{
 					?>
-                    
+
 							<ul>
 								<li><a href="index.php">Home</a></li>
 								<li><a href="status.php">View Status</a></li>
